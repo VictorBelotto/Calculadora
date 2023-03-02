aadb83d822470c67fa4d1fe52fbc335484df43a1
VictorBelotto <victortucunduva.vb@outlook.com>
2023-03-02 11:00:53 -0300

    funcionalidades JS

--- script.js
+++ script.js
@@ -1,46 +1,85 @@
 const compraEVenda = document.querySelector("#compraEVenda")
 const financiamento = document.querySelector("#financiamento")
+const selectBanco = document.querySelector("#selectBanco")
+const selectCidade = document.querySelector("#selectCidade")
+const selectEnquadramento = document.querySelector("#selectEnquadramento")
 const botaoCalcular = document.querySelector("#botaoCalcular")
+const outputDados = document.querySelector("#outputDados")
+
 const enquadramento = document.querySelector("#enquadramento")
-const banco = document.querySelector("#selectBanco")
+
 
 class dados {
     compra
     financiamento
     banco
-    recursosProprios
-    constructor(compra, financiamento, banco) {
+    cidade
+    enquadramento
+    
+    constructor(compra, financiamento, banco, cidade, enquadramento) {
         this.compra = compra
         this.financiamento = financiamento
         this.banco = banco
+        this.cidade = cidade
+        this.enquadramento = enquadramento
     }
 
-    getRecursosProprios(){
-        return this.recursosProprios
-    }
+   
 }
 
-botaoCalcular.addEventListener('click', () => {
-    const novoDado = new dados(compraEVenda.valueAsNumber, financiamento.valueAsNumber, banco.valueAsNumber)
-    
-    recursosProprios(novoDado)
+class taxasCalculadas {
+    cartorio
+    itbi 
+    vistoria
+    relacionamento
+}
 
-    verificaBanco(banco.value)
+class limiteMCMV {
+    campinas = 264000
+    guarulhos = 264000
+}
+
+
+botaoCalcular.addEventListener('click', () => {
+    const dado = new dados(compraEVenda.valueAsNumber, financiamento.valueAsNumber, selectBanco.value, selectCidade.value, selectEnquadramento.value)
+    const taxa = new taxasCalculadas()
+   
+
+    referenciaTaxas(dado, taxa)
+
+    console.log(dado, taxa)
 
 })
 
 
-function recursosProprios(dado) {
-    return dado.recursosProprios = "compra - financiamento"
+function referenciaTaxas(dado, taxa){
+    dado.recursosProprios = dado.compra - dado.financiamento
+
+    if(dado.cidade == "campinas"){
+        taxa.relacionamento = 1500
+        taxa.itbi = dado.compra * 0.027
+    }
+        
+        else if(dado.cidade == "guarulhos"){
+            taxa.relacionamento = 500
+            taxa.itbi = (dado.financiamento * 0.005) + (dado.recursosProprios * 0.02)
+        }
+    
 }
+
+
+function verificaBanco(banco){
+    if(banco == "caixa"){
+        return true
+    }
+}
+
+
 
 
 function trocaDisplay(referencia){
     referencia.classList.toggle("display-none")
 }
 
-function verificaBanco(banco){
-    if(banco == "caixa"){
-        trocaDisplay(enquadramento)
-    }
-}
+
+
