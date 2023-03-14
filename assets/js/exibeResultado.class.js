1cb0313f1a4b60bef2ed5a7628ddd629e60c97a2
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2023-03-14 14:00:48 -0300

    update css

--- assets/js/exibeResultado.class.js
+++ assets/js/exibeResultado.class.js
@@ -10,34 +10,34 @@ export function exibeResultado(taxa){
     
     createUl()
     
-    createLi(`Valor do Cartorio: ${modificaDinheiroReal(taxa.cartorio)}`)
-    createLi(`Valor do ITBI: ${modificaDinheiroReal(taxa.itbi)}`)
-    createLi(`Valor do Vistoria: ${modificaDinheiroReal(taxa.vistoria)}`)
+    createLi(`Valor do Cartorio: ${modificaDinheiroReal(taxa.cartorio)}`, 'resultado')
+    createLi(`Valor do ITBI: ${modificaDinheiroReal(taxa.itbi)}` , 'resultado')
+    createLi(`Valor do Vistoria: ${modificaDinheiroReal(taxa.vistoria)}`,  'resultado')
 
     if(taxa.relacionamento != null){
-        createLi(`Valor do Relacionamento: ${modificaDinheiroReal(taxa.relacionamento)}`)
+        createLi(`Valor do Relacionamento: ${modificaDinheiroReal(taxa.relacionamento)}`,  'resultado')
     }
     
 }
 
-export function exibeErros(erro){
+export function exibeErros(resultado){
    outputDados.firstChild.remove()
    outputDados.classList.remove("display-none")
 
    createUl()
 
-   createLi(erro)
+   createLi(resultado, 'erro')
 }
 
  function createUl(){
     let ul = document.createElement("ul")
-    ul.classList.add("lista")
     outputDados.appendChild(ul)
  }
 
-export function createLi(resultado){
+export function createLi(resultado, classe){
 
     let li = document.createElement("li")
+    li.classList.add(classe);
     li.textContent = resultado
-    outputDados.querySelector(".lista").appendChild(li)
+    outputDados.querySelector("ul").appendChild(li)
 }
